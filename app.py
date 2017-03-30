e099fda55181bfc67b6173034bc2155e6150b28a
Marco Hemken <mehemken@gmail.com>
2017-03-29 20:23:49 -0700

    The app is now back to when it worked.

--- app.py
+++ app.py
@@ -3,23 +3,32 @@
 import sys
 import os
 import logging
-from logging.handlers import RotatingFileHandler
 
+from logging.handlers import RotatingFileHandler
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-b', '--build',
+        help='Build the static site.',
+        action='store_true')
 
 from flask import Flask, render_template
 from flask_flatpages import FlatPages
 from flask_frozen import Freezer
 
-os.chdir('..')
-BUILD_PATH = os.getcwd()
-os.chdir('app/')
+# os.chdir('..')
+# BUILD_PATH = os.getcwd()
+# os.chdir('app/')
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
 FLATPAGES_EXTENSION = '.md'
-FREEZER_DESTINATION = BUILD_PATH
+
+build_path = '~/Documents/git/mehemken.io/build/'
+result = os.path.abspath(build_path)
+FREEZER_DESTINATION = result
 
 
 app = Flask(__name__)
@@ -27,24 +36,28 @@ app.config.from_object(__name__)
 pages = FlatPages(app)
 freezer = Freezer(app)
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/about/')
 def about():
     page = pages.get_or_404('about')
-    return render_template('about.html', page=page)
+    return render_template('default.html', page=page)
 
-if __name__ == "__main__":
-    handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
+if __name__ == '__main__':
+    handler = RotatingFileHandler('logs/mehemken.io.log', maxBytes=10000, backupCount=1)
     handler.setLevel(logging.INFO)
     app.logger.addHandler(handler)
-    if len(sys.argv) > 1 and sys.argv[1] == "build":
-        ans = input('continue? ')
+    args = parser.parse_args()
+
+    if args.build:
+        app.logger.info('Do you want to build?')
+        ans = input('yes/no: ')
         if ans == 'yes':
-            freezer.freeze()
+            # freezer.freeze()
+            app.logger.debug('This is where the build happens.')
         else:
-            logger.info('build aborted')
+            app.logger.info('build aborted')
     else:
         app.run(debug=True)
---
+++ pages/about.md
@@ -0,0 +1,4 @@
+title: hello world
+date: 2017-3-29
+
+**hello world**, from a *page*!
