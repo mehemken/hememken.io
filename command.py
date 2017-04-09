6762275d39536dcef7dc86632052ff0347604af1
Marco Hemken <mehemken@gmail.com>
2017-04-09 11:47:14 -0700

    Starting to move CLI to new file

--- command.py
+++ command.py
@@ -4,19 +4,22 @@ import sys
 import os
 import logging
 
-from app import freezer
+from app import freezer, FREEZER_DESTINATION
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
-# import argparse
-# parser = argparse.ArgumentParser()
-# parser.add_argument('-b', '--build',
-#         help='Build the static site.',
-#         action='store_true')
-# parser.add_argument('-p', '--path',
-#         help='Print the build path.',
-#         action='store_true')
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-b', '--build',
+        help='Build the static site.',
+        action='store_true')
+parser.add_argument('-p', '--path',
+        help='Print the build path.',
+        action='store_true')
+parser.add_argument('-c', '--commit',
+        help='Commit latest changes to build master branch.',
+        action='store_true')
 
 
 
@@ -24,18 +27,24 @@ if __name__ == '__main__':
 
     logger.info('foo bar baz')
 
-#     args = parser.parse_args()
-#
-#     if args.path:
-#         app.logger.info(FREEZER_DESTINATION)
-#         sys.exit()
-#
-#     if args.build:
-#         app.logger.info('Freezing the current site at\n{}'.format(FREEZER_DESTINATION))
-#         try:
-#             freezer.freeze()
-#             app.logger.info('Freeze completed successfully.')
-#         except:
-#             app.logger.exception('There was an exception during the freeze.')
-#         finally:
-#             sys.exit('bye')
+    args = parser.parse_args()
+
+    if args.commit:
+        logger.info('This commits all changes to build master.')
+        """
+        Some code here.
+        """
+
+    if args.path:
+        logger.info(FREEZER_DESTINATION)
+        sys.exit()
+
+    if args.build:
+        logger.info('Freezing the current site at\n{}'.format(FREEZER_DESTINATION))
+        try:
+            freezer.freeze()
+            logger.info('Freeze completed successfully.')
+        except:
+            logger.exception('There was an exception during the freeze.')
+        finally:
+            sys.exit('bye')
